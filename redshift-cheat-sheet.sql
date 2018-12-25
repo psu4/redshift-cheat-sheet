64b84507afd818b304686ae4237e3b0cac2be1fb
Pin-Chih Su <psu@marqeta.com>
2018-12-25 11:55:12 -0800

    2018-12-25 changes

---
+++ redshift-cheat-sheet.sql
@@ -0,0 +1,35 @@
+-- By Pin-Chih Su
+
+-- 1. table tips
+
+-- 1.a. alter table
+
+ALTER TABLE mqt_tabviews.fraud_rule_violations ADD COLUMN mcc_description varchar(1000) default NULL;
+
+-- 1.b. check user permissions
+
+SELECT
+    u.usename,
+    s.schemaname,
+    has_schema_privilege(u.usename,s.schemaname,'create') AS user_has_select_permission,
+    has_schema_privilege(u.usename,s.schemaname,'usage') AS user_has_usage_permission
+FROM
+    pg_user u
+CROSS JOIN
+    (SELECT DISTINCT schemaname FROM pg_tables) s
+WHERE
+    u.usename = 'user-name-variable'
+
+;
+
+-- 1.c. check if a table exists or not
+
+SELECT * FROM information_schema.tables where table_name='table-name';
+
+-- 1.d. check if a column exists or not
+
+SELECT EXISTS (
+
+SELECT * FROM information_schema.columns where table_name='name' and column_name='name' ); -- return true or false
+
+
