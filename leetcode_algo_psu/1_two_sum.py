c77bda9df1ceff4cac5561d461bf1228adaea219
Pin-Chih Su <psu@marqeta.com>
2019-09-24 22:13:13 -0700

    two sum questions

---
+++ leetcode_algo_psu/1_two_sum.py
@@ -0,0 +1,23 @@
+nums = [2, 7, 11, 15]
+target = int(9)
+
+class Solution:
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
+
+
+d=Solution().twoSum
+
+print(d(nums,target))
